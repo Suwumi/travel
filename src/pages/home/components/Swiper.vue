5b1ca985f6c825a5f54a92760937f72130b2cdd0
ssh <1124189934@qq.com>
2019-05-13 14:39:00 +0800

    'change'

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img 
+                class="swiper-img" 
+                :src="item.imgUrl" 
+            />
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+        <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+        <!--<div class="swiper-button-next" slot="button-next"></div>-->
+        <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+        </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data() {
+        return{
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id: '0001',
+                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/b23a39921e8b78f38b61412d691d93ea.jpg_750x200_942ed7bd.jpg'
+            },{
+                id: '0002',
+                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/5558e5b96782e91c01c52163bd315253.jpg_750x200_5031d0cc.jpg'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        .swiper-img
+            width: 100%
+</style>
