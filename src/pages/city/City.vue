8169d39923bddf5c0e870c69139b997b7d5c2bd3
ssh <1124189934@qq.com>
2019-05-14 19:47:45 +0800

    'cityajax'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 
@@ -23,8 +23,27 @@ export default {
     },
     data () {
         return {
-            
+            cities: {},
+            hotCities: []
         }
+    },
+    methods: {
+        getCityInfo () {
+            axios.get('/api/city.json')
+                .then(this.CityInfoSucc)
+        },
+        CityInfoSucc (res) {
+            console.log(res)
+            res = res.data
+            if(res.ret && res.data) {
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+        }
+    },
+    mounted () {
+        this.getCityInfo()
     }
 }
 </script>
